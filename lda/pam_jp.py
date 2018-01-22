484f6b7df251ab46dbd8886915c937cb83c3c14b
hiroki-iida <hiroki-iida@mbp-2016-h-iida.local>
2018-01-22 15:50:39 +0900

    first pam

--- lda/pam_jp.py
+++ lda/pam_jp.py
@@ -10,11 +10,11 @@ import numpy as np
 
 
 class PAM:
-    def __init__(self, S, K, alphas, alphak, beta, docs, V, smartinit=True):
+    def __init__(self, S, K, alphas, beta, docs, V, smartinit=True):
         self.S = S  # super topic num
         self.K = K  # sub topic num
         self.alphas = np.ones(S) * alphas  # parameter of super-topic prior
-        self.alphask = np.ones(S, K) * alphak  # parameter of sub-topic prior
+        self.alphask = np.ones(S, K) * alphas / K  # parameter of sub-topic prior
         self.beta = np.ones(K) * beta   # parameter of words prior
         self.docs = docs  # matrix of each word id for each docs
         self.V = V  # Number of all vocaburary
@@ -30,21 +30,28 @@ class PAM:
         # count of each topic and vocabulary
         self.n_zk_t = np.zeros((K, V))
         # topic count of each topic
-        self.n_zk = np.zeros(K)
-        self.N = len(doc)
+        self.n_zk = np.zeros(K) + V * beta
+        self.N = len(docs)
         for m, doc in enumerate(docs):
             self.N += len(doc)  # all word
-            zs_n = []  # super-topic of nth word in mth doc
-            zk_n = []  # sub-topic of nth word in mth doc
+            zs_j = []  # super-topic of jth word in mth doc
+            zk_j = []  # sub-topic of jth word in mth doc
             for t in doc:  # t is id of word
                 if smartinit:
-                    n_s = self.n_m_zs[m] + self.alphas
+                    """
+                    n_s = self.n_m_zs[m] + self.alphas   # mth doc, S vec
                     p_s = n_s / np.sum(n_s)
-                    n_k = self.n_m_zk[m] + self.alphask
-                    p_k = n_k / np.sum(n_k, axis=1)
-                    n_v = self.n_zk_t[t] + self.beta
+                    n_k = self.n_m_zk[m] + self.alphask  # mth doc, SxK matrix
+                    p_k = n_k / (self.n_m_zs + self.alphas)
+                    n_v = self.n_zk_t[:, t] + self.beta  # tth word, K vec
                     p_v = n_v / (self.n_zk + self.beta)
                     p_zsk = p_s * p_k * p_v
+                    """
+                    n_k = self.n_m_zk + self.alphask
+                    n_v = self.n_zk_t[t] + self.beta
+                    p_zsk = n_k * n_v /
+                            (len(self.docs[m]) + np.sum(self.alphas)) \
+                            / self.n_zk
 
                     p_zs = np.sum(p_zsk, axis=1)
                     p_zk = np.sum(p_zsk, axis=0)
@@ -57,21 +64,21 @@ class PAM:
                     zk = np.random.randint(0, K)
 
                 self.n_m_zs[m, zs] += 1  # set super topic count on each doc
-                zs_n.append(zs)
-                self.n_m_zk[m, zk] += 1  # set sub topic count on each doc
-                zk_n.append(zk)  # set topic on each word
+                zs_j.append(zs)
+                self.n_m_zk[m, zs, zk] += 1  # set sub topic count on each doc
+                zk_j.append(zk)  # set topic on each word
                 self.n_zk_t[zk, t] += 1  # set wordcount on each topic
                 self.n_zk[zk] += 1  # set topic distribution
-            self.zs_m_n.append(np.array(zs_n))
-            self.zk_m_m.append(np.array(zk_n))
+            self.zs_m_j.append(np.array(zs_j))
+            self.zk_m_j.append(np.array(zk_j))
 
     def inference(self):
         """learning once iteration using collapsed Gibbs Sampling"""
         for m, doc in enumerate(self.docs):
             # Be careful followings are views
             # So self.hoge will be change, when changing variant
-            zs_j = self.zs_m_n[m]
-            zk_j = self.zk_m_n[m]
+            zs_j = self.zs_m_j[m]
+            zk_j = self.zk_m_j[m]
             n_m_zs = self.n_m_zs[m]
             n_m_zk = self.n_m_zk[m]
             for j, t in enumerate(doc):
@@ -84,27 +91,32 @@ class PAM:
                 self.n_z[zk] -= 1
 
                 # sampling topic new_z for t
-                n_s = n_m_zs + self.alphas
+                """
+                n_s = n_m_zs + self.alphas   # mth doc, S vec
                 p_s = n_s / np.sum(n_s)
-                n_k = n_m_zk + self.alphask
-                p_k = n_k / np.sum(n_k, axis=1)
-                n_v = self.n_zk_t[t] + self.beta
-                p_v = n_v / (self.n_z[zk] + self.beta)
+                n_k = n_m_zk + self.alphask  # mth doc, SxK matrix
+                p_k = n_k / n_s
+                n_v = self.n_zk_t[:, t] + self.beta
+                p_v = n_v / (self.n_zk + self.beta)
                 p_zsk = p_s * p_k * p_v  # SxK matrix
+                """
 
+                n_k = n_m_zk + self.alphask
+                n_v = self.n_zk_t[t] + self.beta
+                p_zsk = n_k * n_v \
+                        / (len(self.docs[m])+np.sum(self.alphas)) \
+                        / self.n_zk
                 p_zs = np.sum(p_zsk, axis=1)
                 p_zk = np.sum(p_zsk, axis=0)
 
                 new_zs = np.random.multinomial(1, p_zs).argmax()
                 new_zk = np.random.multinomial(1, p_zk).argmax()
 
-
                 # set z the new topic and increment counters
                 zs_j[j] = new_zs
                 zk_j[j] = new_zk
                 n_m_zs[new_zs] += 1
-                n_m_zk[new_zk] += 1
-                self.n_m_zs
+                n_m_zk[new_zs, new_zk] += 1
                 self.n_zk_t[new_zk, t] += 1
                 self.n_zk[new_zk] += 1
 
@@ -117,7 +129,7 @@ class PAM:
 
     def worddist(self):
         """get topic-word distribution"""
-        return self.n_z_t / self.n_z[:, np.newaxis]
+        return self.n_zk_t / self.n_zk[:]
 
     def perplexity(self, docs=None):
         if docs is None:
@@ -125,35 +137,45 @@ class PAM:
         phi = self.worddist()
         log_per = 0
         N = 0
-        Kalpha = self.K * self.alpha
         for m, doc in enumerate(docs):
-            theta = self.n_m_z[m] / (len(self.docs[m]) + Kalpha)
+            """
+            thetas = (self.n_m_zs[m] + self.alphas) \
+                     / (len(self.docs[m]) + np.sum(self.alphas))
+            thetask = (self.n_m_zk[m] + self.alphask) \
+                     / (self.n_m_zs[m] + self.alphas)
+            theta = np.dot(thetas, thetask)
+            """
+            theta = np.sum((self.n_m_zk[m] + self.alphask) /
+                    (len(self.docs[m]) + np.sum(self.alphas), axis = 0)
             for w in doc:
-                log_per -= np.log(np.inner(phi[:, w], theta))
+                log_per -= np.log(np.dot(theta, phi[:, w]))
             N += len(doc)
         return np.exp(log_per / N)
 
 
-def pam_learning(lda, iteration, voca):
-    pre_perp = lda.perplexity()
+
+def pam_learning(pam, iteration, voca, hp):
+    pre_perp = pam.perplexity()
     print("initial perplexity=%f" % pre_perp)
     for i in range(iteration):
-        lda.inference()
-        perp = lda.perplexity()
+        pam.inference()
+        perp = pam.perplexity()
+        if hp:
+            pam.hyper_parameter_inference()
         print("-%d p=%f" % (i + 1, perp))
         if pre_perp:
             if pre_perp < perp:
-                output_word_topic_dist(lda, voca)
+                output_word_topic_dist(pam, voca)
                 pre_perp = None
             else:
                 pre_perp = perp
-    output_word_topic_dist(lda, voca)
+    output_word_topic_dist(pam, voca)
 
 
-def output_word_topic_dist(lda, voca):
-    zcount = np.zeros(lda.K, dtype=int)
-    wordcount = [dict() for k in range(lda.K)]
-    for xlist, zlist in zip(lda.docs, lda.z_m_n):
+def output_word_topic_dist(pam, voca):
+    zcount = np.zeros(pam.K, dtype=int)
+    wordcount = [dict() for k in range(pam.K)]
+    for xlist, zlist in zip(pam.docs, pam.z_m_n):
         for x, z in zip(xlist, zlist):
             zcount[z] += 1
             if x in wordcount[z]:
@@ -161,8 +183,8 @@ def output_word_topic_dist(lda, voca):
             else:
                 wordcount[z][x] = 1
 
-    phi = lda.worddist()
-    for k in range(lda.K):
+    phi = pam.worddist()
+    for k in range(pam.K):
         print("\n-- topic: %d (%d words)" % (k, zcount[k]))
         for w in np.argsort(-phi[k])[:20]:
             print("%s: %f (%d)" % (voca[w], phi[k, w], wordcount[k].get(w, 0)))
@@ -180,9 +202,6 @@ def main():
     parser.add_option("--alphas", dest="alphas",
                       type="float", help="parameter \
                       alphas", default=0.5)
-    parser.add_option("--alphak", dest="alphak",
-                      type="float", help="parameter \
-                      alphak", default=0.5)
     parser.add_option("--beta", dest="beta",
                       type="float", help="parameter \
                       beta", default=0.5)
@@ -211,7 +230,7 @@ def main():
     parser.add_option("--df", dest="df", type="int",
                       help="threshold of document \
                       freaquency to cut words", default=0)
-    parser.add_option("-e", dest="e", action='store_true',
+    parser.add_option("-hp", dest="hp", action='store_true',
                       help="hyper parameter inference")
     (options, args) = parser.parse_args()
     if not (options.dirname or options.filename or options.corpus):
@@ -234,15 +253,15 @@ def main():
     if options.df > 0:
         docs = voca.cut_low_freq(docs, options.df)
 
-    pam = PAM(options.S, options.K, options.alphas, options.alphak,
-              options.beta, docs, voca.size(), options.smartinit)
+    pam = PAM(options.S, options.K, options.alphas, options.beta,
+              docs, voca.size(), options.smartinit)
     print("corpus=%d, words=%d, K=%d, a=%f, b=%f"
           % (len(corpus), len(voca.vocas), options.K,
              options.alpha, options.beta))
 
 # import cProfile
 # cProfile.runctx('lda_learning(lda, options.iteration, voca)', globals(),locals(), 'lda.profile')
-    pam_learning(pam, options.iteration, voca)
+    pam_learning(pam, options.iteration, voca, options.hp)
 
 
 if __name__ == "__main__":
